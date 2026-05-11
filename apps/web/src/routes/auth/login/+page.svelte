3f3d0eb4e585cdd8d5d29991969fc58550534ed9
ukihwa <ukihwa@gmail.com>
2026-05-11 11:58:10 +0900

    fix(web/login): narrower tooltip arrow via css triangle
    rotate-45 size-3 square로 그리던 화살표는 폭(대각선 ~17px) 대비 길이가
    짧음. border-x-transparent + border-t로 폭 10px / 길이 8px 삼각형으로
    교체해 폭은 줄이고 시각적 길이는 유지.

--- apps/web/src/routes/auth/login/+page.svelte
+++ apps/web/src/routes/auth/login/+page.svelte
@@ -64,7 +64,7 @@
 						<div class="flex justify-end pr-4">
 							<div class="relative rounded-md bg-foreground px-3.5 py-2 text-[13px] font-medium text-background">
 								최근에 사용했어요
-								<span class="absolute -bottom-1.5 right-6 size-3 rotate-45 bg-foreground"></span>
+								<span class="absolute -bottom-2 right-6 h-0 w-0 border-x-[5px] border-t-[8px] border-x-transparent border-t-foreground"></span>
 							</div>
 						</div>
 					{/if}
@@ -83,7 +83,7 @@
 						{#if lastLoginMethod === 'kakao'}
 							<div class="absolute -top-11 left-1/2 -translate-x-1/2 whitespace-nowrap rounded-md bg-foreground px-3.5 py-2 text-[13px] font-medium text-background">
 								최근에 사용했어요
-								<span class="absolute -bottom-1.5 left-1/2 size-3 -translate-x-1/2 rotate-45 bg-foreground"></span>
+								<span class="absolute -bottom-2 left-1/2 h-0 w-0 -translate-x-1/2 border-x-[5px] border-t-[8px] border-x-transparent border-t-foreground"></span>
 							</div>
 						{/if}
 						<button
@@ -104,7 +104,7 @@
 						{#if lastLoginMethod === 'naver'}
 							<div class="absolute -top-11 left-1/2 -translate-x-1/2 whitespace-nowrap rounded-md bg-foreground px-3.5 py-2 text-[13px] font-medium text-background">
 								최근에 사용했어요
-								<span class="absolute -bottom-1.5 left-1/2 size-3 -translate-x-1/2 rotate-45 bg-foreground"></span>
+								<span class="absolute -bottom-2 left-1/2 h-0 w-0 -translate-x-1/2 border-x-[5px] border-t-[8px] border-x-transparent border-t-foreground"></span>
 							</div>
 						{/if}
 						<button
@@ -125,7 +125,7 @@
 						{#if lastLoginMethod === 'google'}
 							<div class="absolute -top-11 left-1/2 -translate-x-1/2 whitespace-nowrap rounded-md bg-foreground px-3.5 py-2 text-[13px] font-medium text-background">
 								최근에 사용했어요
-								<span class="absolute -bottom-1.5 left-1/2 size-3 -translate-x-1/2 rotate-45 bg-foreground"></span>
+								<span class="absolute -bottom-2 left-1/2 h-0 w-0 -translate-x-1/2 border-x-[5px] border-t-[8px] border-x-transparent border-t-foreground"></span>
 							</div>
 						{/if}
 						<button
